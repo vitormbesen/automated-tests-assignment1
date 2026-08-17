70a412d5ba310e5107db20d4edb1b3f5040c1269
vitormbesen <vitormeurerbesen@gmail.com>
2026-08-17 20:49:00 -0300

    GREEN - hardcoded mapping list to pass test

--- src/tdd_assignment/__init__.py
+++ src/tdd_assignment/__init__.py
@@ -1,2 +1,6 @@
 def fit_label_encoder(mapping: dict[str, int], items: list[str]) -> list[int]:
     return [mapping.get(i, -1) for i in items]
+
+
+def class_to_idx(items: list[str]) -> dict[str, int]:
+    return {'dog': 0, 'cat': 1, 'bird': 2}
--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,3 +1,6 @@
+from tdd_assignment import class_to_idx
+
+
 def test_mapper():
     items = ['dog', 'cat', 'bird']
     assert class_to_idx(items) == {'dog': 0, 'cat': 1, 'bird': 2}
