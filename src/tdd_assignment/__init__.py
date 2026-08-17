48e28da4e7f0a46889ab644f38a3a283ecbdef91
vitormbesen <vitormeurerbesen@gmail.com>
2026-08-17 20:40:19 -0300

    REFACTOR(GREEN) - Correct implementation for handling non-mapped classes

--- src/tdd_assignment/__init__.py
+++ src/tdd_assignment/__init__.py
@@ -1,2 +1,2 @@
 def fit_label_encoder(mapping: dict[str, int], items: list[str]) -> list[int]:
-    return [mapping[i] if i != 'airplane' else -1 for i in items]
+    return [mapping.get(i, -1) for i in items]
