bf9ec760272ccb0cda53c559b44c1cb05f1575c3
vitormbesen <vitormeurerbesen@gmail.com>
2026-08-18 14:15:46 -0300

    FAIL - introduce bug in function to showcase tests capture it

--- src/tdd_assignment/__init__.py
+++ src/tdd_assignment/__init__.py
@@ -4,3 +4,8 @@ def fit_label_encoder(mapping: dict[str, int], items: list[str]) -> list[int]:
 
 def class_to_idx(items: list[str]) -> dict[str, int]:
     return {item: idx for idx, item in enumerate(sorted(set(items)))}
+
+
+# Buggy version: does not sort - Will fail properties
+def class_to_idx(items: list[str]) -> dict[str, int]:
+    return {item: idx for idx, item in enumerate(set(items))}
